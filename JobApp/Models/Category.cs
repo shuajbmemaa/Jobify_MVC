f3322ac9d254eba092eddbb4223693e752ee1fb0
shuajbkingu <shuajbmemaa@hotmail.com>
2024-01-01 16:55:53 +0100

    Category Model

---
+++ JobApp/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace JobApp.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+    }
+}
