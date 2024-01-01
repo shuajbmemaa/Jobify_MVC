41667bea648849f1ce8ad239f26df8974325fee5
shuajbkingu <shuajbmemaa@hotmail.com>
2024-01-01 16:59:56 +0100

    Data Annotations

--- JobApp/Models/Category.cs
+++ JobApp/Models/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace JobApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobApp.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
 
         public int DisplayOrder { get; set; }
