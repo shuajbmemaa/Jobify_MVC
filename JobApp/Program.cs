3369df504813faa8f3d224d4a536a8786a0a9e91
shuajbkingu <shuajbmemaa@hotmail.com>
2024-01-14 12:00:37 +0100

    JobApp

--- JobApp/Program.cs
+++ JobApp/Program.cs
@@ -1,7 +1,17 @@
+using Jobify.DataAccess.Data;
+using Jobify.DataAccess.Repository;
+using Jobify.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
 
 var app = builder.Build();
 
@@ -22,6 +32,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
