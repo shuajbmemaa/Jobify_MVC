54b8db43cbdecb1a013186cb3cc54e92146160c8
shuajbkingu <shuajbmemaa@hotmail.com>
2024-01-14 12:01:18 +0100

    Jobify Models

---
+++ Jobify.Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Jobify.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Emri i kategorise")]
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        [DisplayName("Display Order")]
+        [Range(1, 100)]
+        public int DisplayOrder { get; set; }
+    }
+}
