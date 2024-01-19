fe26515dc9fa534803e1bdef4060ce60b44c1dd1
shuajbkingu <shuajbmemaa@hotmail.com>
2024-01-19 17:41:35 +0100

    AddImageUrl

--- Jobify.DataAccess/Data/ApplicationDbContext.cs
+++ Jobify.DataAccess/Data/ApplicationDbContext.cs
@@ -22,9 +22,9 @@ namespace Jobify.DataAccess.Data
                 new Category { Id = 3, Name = "Full-Stack", DisplayOrder = 3 }
                 );
             modelBuilder.Entity<Punet>().HasData(
-            new Punet { Id = 1, Name = "Punë 1", Description = "Përshkrimi 1", Kerkesat = "Kërkesat 1", Lokacioni = "Lokacioni 1",kategoria="Administrate" },
-            new Punet { Id = 2, Name = "Punë 2", Description = "Përshkrimi 2", Kerkesat = "Kërkesat 2", Lokacioni = "Lokacioni 2",kategoria="Teknologji"},
-            new Punet { Id = 3, Name = "Punë 3", Description = "Përshkrimi 3", Kerkesat = "Kërkesat 3", Lokacioni = "Lokacioni 3",kategoria="Biznis"}
+            new Punet { Id = 1, Name = "Punë 1", Description = "Përshkrimi 1", Kerkesat = "Kërkesat 1", Lokacioni = "Lokacioni 1",kategoria="Administrate",ImageUrl="" },
+            new Punet { Id = 2, Name = "Punë 2", Description = "Përshkrimi 2", Kerkesat = "Kërkesat 2", Lokacioni = "Lokacioni 2",kategoria="Teknologji", ImageUrl = "" },
+            new Punet { Id = 3, Name = "Punë 3", Description = "Përshkrimi 3", Kerkesat = "Kërkesat 3", Lokacioni = "Lokacioni 3",kategoria="Biznis", ImageUrl = "" }
     );
 
 
