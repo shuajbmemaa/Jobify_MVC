4585d37ad5b901c72dd7b73ff2fc5f3df83758b2
shuajbkingu <shuajbmemaa@hotmail.com>
2024-01-24 14:43:55 +0100

    imageUrl

--- Jobify.Models/Punet.cs
+++ Jobify.Models/Punet.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Http;
 
 namespace Jobify.Models
 {
