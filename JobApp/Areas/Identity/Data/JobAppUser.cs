ed293746d90ea07e6b4ee4e539a46fb4f9fbdd2d
RigonP <rp57421@ubt-uni.net>
2024-01-25 16:11:56 +0100

    Shtimi i dy variablave te Registration

--- JobApp/Areas/Identity/Data/JobAppUser.cs
+++ JobApp/Areas/Identity/Data/JobAppUser.cs
@@ -9,6 +9,7 @@ namespace JobApp.Areas.Identity.Data;
 // Add profile data for application users by adding properties to the JobAppUser class
 public class JobAppUser : IdentityUser
 {
+    //Kemi shtuar dy variabla te ndryshme 
     public string FirstName { get; set; }
     public string LastName { get; set; }
 }
