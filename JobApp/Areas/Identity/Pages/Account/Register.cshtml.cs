a00f29f15f8bc8793805d17454f56dee9c739a0b
RigonP <rp57421@ubt-uni.net>
2024-01-27 22:38:12 +0100

    Definimi i rolit ne InputModel

--- JobApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ JobApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -74,6 +74,9 @@ namespace JobApp.Areas.Identity.Pages.Account
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
+        //public List<string> SelectedRoles { get; set; }
+
+
         public class InputModel
         {
 			[Required]
@@ -112,6 +115,9 @@ namespace JobApp.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+           
+            public List<string> SelectedRoles { get; set; }
         }
 
 
@@ -139,7 +145,7 @@ namespace JobApp.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
-
+          
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
