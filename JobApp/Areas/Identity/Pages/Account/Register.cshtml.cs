e51bcaf8b3b41915e4f50cd388123a7888c3f4d5
RigonP <rp57421@ubt-uni.net>
2024-01-27 22:39:22 +0100

    Marrja e rolit te perzgjedhur nga perdoruesi gjate procesit te regjistrimit

--- JobApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ JobApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -146,6 +146,22 @@ namespace JobApp.Areas.Identity.Pages.Account
                 {
                     _logger.LogInformation("User created a new account with password.");
           
+                    // Assign roles to user upon registration
+                    var selectedRoles = Input.SelectedRoles; // Retrieve selected roles from data
+                    foreach (var roleName in selectedRoles)
+                    {
+                        var roleResult = await _userManager.AddToRoleAsync(user, roleName);
+                        if (!roleResult.Succeeded)
+                        {
+                            foreach (var error in roleResult.Errors)
+                            {
+                                ModelState.AddModelError(string.Empty, error.Description);
+                            }
+                            return Page();
+                        }
+                    }
+
+
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
