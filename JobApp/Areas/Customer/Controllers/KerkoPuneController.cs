8accde97f130b9df1082abbbebdb4335c6e7cb46
Erzaaaaa <erzaa.17@hotmail.com>
2024-01-28 13:47:41 +0100

    KerkoPuneController

---
+++ JobApp/Areas/Customer/Controllers/KerkoPuneController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobApp.Areas.Customer.Controllers
+{
+    public class KerkoPuneController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
