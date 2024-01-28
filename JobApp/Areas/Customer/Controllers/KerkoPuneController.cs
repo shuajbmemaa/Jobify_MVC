5716bd00e31009f6ce7bd65782b633d5e8401bd0
Erzaaaaa <erzaa.17@hotmail.com>
2024-01-28 13:49:08 +0100

    Shtim i metodave

--- JobApp/Areas/Customer/Controllers/KerkoPuneController.cs
+++ JobApp/Areas/Customer/Controllers/KerkoPuneController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Jobify.DataAccess.Repository.IRepository;
+using Jobify.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JobApp.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class KerkoPuneController : Controller
     {
+        private readonly ILogger<KerkoPuneController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public KerkoPuneController(ILogger<KerkoPuneController> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Punet> listaPunes = _unitOfWork.Punet.GetAll();
+            return View(listaPunes);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Punet puna = _unitOfWork.Punet.Get(u => u.Id == id);
+            return View(puna);
         }
     }
 }
