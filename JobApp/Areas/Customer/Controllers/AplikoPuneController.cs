f079a1d7ed4ead90d4931e372a8dc56038576aad
RigonP <rp57421@ubt-uni.net>
2024-01-28 21:27:59 +0100

    Krijimi i formularit per aplikim

---
+++ JobApp/Areas/Customer/Controllers/AplikoPuneController.cs
@@ -0,0 +1,35 @@
+﻿using Jobify.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Jobify.Models;
+
+namespace JobApp.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class AplikoPuneController : Controller
+    {
+        private readonly ILogger<AplikoPuneController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AplikoPuneController(ILogger<AplikoPuneController> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+        [HttpPost]
+        public IActionResult Apply(JobApplicationViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Process the application (e.g., send email, save to database, etc.)
+                /// Set a confirmation message to be displayed
+                ViewBag.ConfirmationMessage = "Your application has been submitted successfully!";
+                return View("Index");
+            }
+            return View("Index", model);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
