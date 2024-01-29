7856bfd0b54b03b962f323f99ad204b5cc96303d
RigonP <rp57421@ubt-uni.net>
2024-01-29 19:03:11 +0100

    Shfaqja e aplikanteve qe kane aplikuar per pune

--- Jobify.DataAccess/Data/ApplicationDbContext.cs
+++ Jobify.DataAccess/Data/ApplicationDbContext.cs
@@ -13,7 +13,7 @@ namespace Jobify.DataAccess.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Aplikimi> Aplikimet { get; set; }
         public DbSet<Punet> Punet { get; set; }
-
+        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Category>().HasData(
